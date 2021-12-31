7c97d4c2ef6250849fab51c5992253569a3fdf91
jjwwk <jjwwk9302@gmail.com>
2021-12-31 22:32:34 +0900

    bug fixed tab1

--- app/src/main/java/com/example/test/Tab1Adapter.kt
+++ app/src/main/java/com/example/test/Tab1Adapter.kt
@@ -17,7 +17,6 @@ import com.example.test.databinding.Tab1ItemRecycleBinding
 class Tab1Adapter(private val datas : MutableList<PhoneBook>,private val context:Context) : RecyclerView.Adapter<Tab1Adapter.ViewHolder>() {
 
 
-
     inner class ViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView) {
         var itemName: TextView = itemView.findViewById(R.id.tab1_name)
         var itemPhone: TextView = itemView.findViewById(R.id.tab1_phone)
@@ -31,6 +30,7 @@ class Tab1Adapter(private val datas : MutableList<PhoneBook>,private val context
             })
             callButton.setOnClickListener(View.OnClickListener {
                 Log.d("where","button clicked!")
+
                 var intent = Intent(Intent.ACTION_CALL)
                 intent.data= Uri.parse("tel:"+itemPhone.text)
 
