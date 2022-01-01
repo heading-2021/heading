7e11ce416014faaa987dc2d5cc3f1d4c2c284fdf
Hans-0101 <hyhsumin@naver.com>
2022-01-01 16:04:18 +0900

    feat:tab3 memory rate update

--- app/src/main/java/com/example/test/Tab3.kt
+++ app/src/main/java/com/example/test/Tab3.kt
@@ -1,59 +1,64 @@
 package com.example.test
 
+import android.app.ActivityManager
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.core.content.ContextCompat.getSystemService
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Tab3.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Tab3 : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var mContext : Context? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is MainActivity) {
+            mContext = context
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab3, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_tab3, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Tab3.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Tab3().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        var text = view.findViewById<TextView>(R.id.test)
+        var btn = view.findViewById<ImageButton>(R.id.imageButton)
+
+        val memoryInfo = ActivityManager.MemoryInfo()
+        (requireActivity().getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager).getMemoryInfo(memoryInfo)
+        val nativeHeapSize = memoryInfo.totalMem
+        val nativeHeapFreeSize = memoryInfo.availMem
+        val usedMemInBytes = nativeHeapSize - nativeHeapFreeSize
+        val usedMemInPercentage = usedMemInBytes * 100 / nativeHeapSize
+
+        text.text= usedMemInPercentage.toString()+"%"
+
+        btn.setOnClickListener{
+            val memoryInfo = ActivityManager.MemoryInfo()
+            (requireActivity().getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager).getMemoryInfo(memoryInfo)
+            val nativeHeapSize = memoryInfo.totalMem
+            val nativeHeapFreeSize = memoryInfo.availMem
+            val usedMemInBytes = nativeHeapSize - nativeHeapFreeSize
+            val usedMemInPercentage = usedMemInBytes * 100 / nativeHeapSize
+
+            text.text= usedMemInPercentage.toString()+"%"
+        }
+
+        return view
     }
 }
+
