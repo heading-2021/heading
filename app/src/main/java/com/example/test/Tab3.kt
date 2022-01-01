606ea42c4bc95d8ead5b14d3fcdd163062554b6b
Hans-0101 <hyhsumin@naver.com>
2022-01-01 20:07:28 +0900

    feat:tab3 memeroy load ver1

--- app/src/main/java/com/example/test/Tab3.kt
+++ app/src/main/java/com/example/test/Tab3.kt
@@ -3,13 +3,20 @@ package com.example.test
 import android.app.ActivityManager
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.MediaController
 import android.widget.TextView
 import androidx.core.content.ContextCompat.getSystemService
+import androidx.databinding.DataBindingUtil.setContentView
+import com.bumptech.glide.Glide
+import pl.droidsonroids.gif.GifDrawable
+import pl.droidsonroids.gif.GifImageView
 
 
 class Tab3 : Fragment() {
@@ -27,6 +34,13 @@ class Tab3 : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
+    fun gifSpeed(percentage:Float): Float {
+        var speed = (percentage/50)*1.2
+        if(speed>0)
+            return speed.toFloat()
+        else
+            return 0.1f
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,6 +51,10 @@ class Tab3 : Fragment() {
 
         var text = view.findViewById<TextView>(R.id.test)
         var btn = view.findViewById<ImageButton>(R.id.imageButton)
+//        var gif = GifDrawable(resources, R.drawable.runner_2)
+        var gif = GifDrawable(resources, R.raw.runner_1)
+        var gif_img = view.findViewById<GifImageView>(R.id.gif)
+        gif_img.setImageDrawable(gif)
 
         val memoryInfo = ActivityManager.MemoryInfo()
         (requireActivity().getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager).getMemoryInfo(memoryInfo)
@@ -45,17 +63,19 @@ class Tab3 : Fragment() {
         val usedMemInBytes = nativeHeapSize - nativeHeapFreeSize
         val usedMemInPercentage = usedMemInBytes * 100 / nativeHeapSize
 
+        gif.setSpeed(gifSpeed(usedMemInPercentage.toFloat()))
         text.text= usedMemInPercentage.toString()+"%"
 
         btn.setOnClickListener{
-            val memoryInfo = ActivityManager.MemoryInfo()
-            (requireActivity().getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager).getMemoryInfo(memoryInfo)
-            val nativeHeapSize = memoryInfo.totalMem
-            val nativeHeapFreeSize = memoryInfo.availMem
-            val usedMemInBytes = nativeHeapSize - nativeHeapFreeSize
-            val usedMemInPercentage = usedMemInBytes * 100 / nativeHeapSize
+            val memoryInfo_2 = ActivityManager.MemoryInfo()
+            (requireActivity().getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager).getMemoryInfo(memoryInfo_2)
+            val nativeHeapSize_2 = memoryInfo_2.totalMem
+            val nativeHeapFreeSize_2 = memoryInfo_2.availMem
+            val usedMemInBytes_2 = nativeHeapSize_2 - nativeHeapFreeSize_2
+            val usedMemInPercentage_2 = usedMemInBytes_2 * 100 / nativeHeapSize_2
 
-            text.text= usedMemInPercentage.toString()+"%"
+            gif.setSpeed(gifSpeed(usedMemInPercentage_2.toFloat()))
+            text.text= usedMemInPercentage_2.toString()+"%"
         }
 
         return view
