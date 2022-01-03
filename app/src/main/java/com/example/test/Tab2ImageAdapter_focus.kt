1fe79840c8a8afeb10d33e51f728a3d1ebb83f43
Hans-0101 <hyhsumin@naver.com>
2022-01-03 15:42:36 +0900

    feat:tab2 zoom fetched

--- app/src/main/java/com/example/test/Tab2ImageAdapter_focus.kt
+++ app/src/main/java/com/example/test/Tab2ImageAdapter_focus.kt
@@ -1,12 +1,14 @@
 package com.example.test
 
 import android.net.Uri
+import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.github.chrisbanes.photoview.PhotoView
 
 class Tab2ImageAdapter_focus(private val items: ArrayList<Uri>, val context: Tab2) : RecyclerView.Adapter<Tab2ImageAdapter_focus.ViewHolder>() {
 
@@ -26,11 +28,19 @@ class Tab2ImageAdapter_focus(private val items: ArrayList<Uri>, val context: Tab
 
     class ViewHolder(v: View) : RecyclerView.ViewHolder(v){
         private var view: View = v
-        var image = v.findViewById<ImageView>(R.id.image_focus)
+        var image = v.findViewById<PhotoView>(R.id.image_focus)
 
         fun bind(listener: View.OnClickListener, item: String){
             view.setOnClickListener(listener)
         }
+
+//        inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+//            override fun onClick(view: View) {
+//                var photoview = view.findViewById<PhotoView>(R.id.photoView)
+//                photoview.setImageResource(R.id.image_focus.toInt())
+//                Log.d("hihhi", "hello")
+//            }
+//        }
     }
 
 }
